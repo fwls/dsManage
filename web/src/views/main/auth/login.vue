64043d5b2887e4ac17b6a84844bf195547267e1a
fs <31260713+fwls@users.noreply.github.com>
2024-07-06 15:15:00 +0800

    Update login.vue

--- web/src/views/main/auth/login.vue
+++ web/src/views/main/auth/login.vue
@@ -1,44 +1,33 @@
 <template>
-  <!-- <div class="login">
-    <n-card title="登录">
-      <n-form ref="formRef" :model="model" label-placement="left" label-width="auto" :style="{
-        maxWidth: '640px',
-      }">
-        <n-form-item label="用户名" path="inputValue" required>
-          <n-input v-model:value="model.username" placeholder="请输入用户名" />
-        </n-form-item>
-        <n-form-item label="密码" path="inputValue" required>
-          <n-input type="password" v-model:value="model.password" placeholder="请输入密码" />
-        </n-form-item>
-
-        <div style="display: flex; justify-content: flex-end">
-          <n-button @click="handleLogin">登录</n-button>
+  <div class="main">
+    <div class="login-box">
+      <form  class="form_main" onsubmit="return false;">
+        <p class="heading">Login</p>
+        <div class="inputContainer">
+          <svg class="inputIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#2e2e2e"
+            viewBox="0 0 16 16">
+            <path
+              d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z">
+            </path>
+          </svg>
+          <input type="text" class="inputField" id="username" v-model="model.username" placeholder="Username">
         </div>
-      </n-form>
 
- 
-    </n-card>
-  </div> -->
-<div class="main">
-  <div class="login-box">
-    <form>
-      <div class="user-box">
-        <input type="text" v-model="model.username"  name="" required="" />
-        <label>用户名</label>
-      </div>
-      <div class="user-box">
-        <input type="password" v-model="model.password" name="" required="" />
-        <label>密码</label>
-      </div>
-      <center>
-        <a href="#" @click="handleLogin">
-          登录
-          <span></span>
-        </a>
-      </center>
-    </form>
+        <div class="inputContainer">
+          <svg class="inputIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#2e2e2e"
+            viewBox="0 0 16 16">
+            <path
+              d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z">
+            </path>
+          </svg>
+          <input type="password" class="inputField" v-model="model.password" id="password" placeholder="Password">
+        </div>
+
+        <button id="button" @click="handleLogin">Submit</button>
+        <!-- <a class="forgotLink" href="#">Forgot your password?</a> -->
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script setup>
@@ -78,111 +67,114 @@ const handleLogin = async () => {
 </script>
 
 <style lang="scss" scoped>
-.n-card {
-  max-width: 320px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 15vh;
-}
-.main {
-  height: 100vh;
-  width: 100vw;
-  background-color: black;
-}
+
 .login-box {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 400px;
-  padding: 40px;
-  transform: translate(-50%, -50%);
-  background: rgba(24, 20, 20, 0.987);
-  box-sizing: border-box;
-  box-shadow: 0 15px 25px rgba(0,0,0,.6);
-  border-radius: 10px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.login-box .user-box {
+.form_main {
+  width: 220px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(255, 255, 255);
+  padding: 30px 30px 30px 30px;
+  box-shadow: 0px 0px 40px rgba(0, 0, 0, 0.062);
   position: relative;
-}
-
-.login-box .user-box input {
-  width: 100%;
-  padding: 10px 0;
-  font-size: 16px;
-  color: #fff;
-  margin-bottom: 30px;
-  border: none;
-  border-bottom: 1px solid #fff;
-  outline: none;
-  background: transparent;
-}
-
-.login-box .user-box label {
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 10px 0;
-  font-size: 16px;
-  color: #fff;
-  pointer-events: none;
-  transition: .5s;
-}
-
-.login-box .user-box input:focus ~ label,
-.login-box .user-box input:valid ~ label {
-  top: -20px;
-  left: 0;
-  color: #bdb8b8;
-  font-size: 12px;
-}
-
-.login-box form a {
-  position: relative;
-  display: inline-block;
-  padding: 10px 20px;
-  color: #ffffff;
-  font-size: 16px;
-  text-decoration: none;
-  text-transform: uppercase;
   overflow: hidden;
-  transition: .5s;
-  margin-top: 40px;
-  letter-spacing: 4px
 }
 
-.login-box a:hover {
-  background: #03f40f;
-  color: #fff;
-  border-radius: 5px;
-  box-shadow: 0 0 5px #03f40f,
-              0 0 25px #03f40f,
-              0 0 50px #03f40f,
-              0 0 100px #03f40f;
-}
-
-.login-box a span {
+.form_main::before {
   position: absolute;
-  display: block;
+  content: "";
+  width: 300px;
+  height: 300px;
+  background-color: rgb(209, 193, 255);
+  transform: rotate(45deg);
+  left: -180px;
+  bottom: 30px;
+  z-index: 1;
+  border-radius: 30px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.082);
 }
 
-@keyframes btn-anim1 {
-  0% {
-    left: -100%;
-  }
-
-  50%,100% {
-    left: 100%;
-  }
+.heading {
+  font-size: 2em;
+  color: #2e2e2e;
+  font-weight: 700;
+  margin: 5px 0 10px 0;
+  z-index: 2;
 }
 
-.login-box a span:nth-child(1) {
-  bottom: 2px;
-  left: -100%;
+.inputContainer {
   width: 100%;
-  height: 2px;
-  background: linear-gradient(90deg, transparent, #03f40f);
-  animation: btn-anim1 2s linear infinite;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+}
+
+.inputIcon {
+  position: absolute;
+  left: 3px;
+}
+
+.inputField {
+  width: 100%;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  border-bottom: 2px solid rgb(173, 173, 173);
+  margin: 10px 0;
+  color: black;
+  font-size: .8em;
+  font-weight: 500;
+  box-sizing: border-box;
+  padding-left: 30px;
+}
+
+.inputField:focus {
+  outline: none;
+  border-bottom: 2px solid rgb(199, 114, 255);
+}
+
+.inputField::placeholder {
+  color: rgb(80, 80, 80);
+  font-size: 1em;
+  font-weight: 500;
+}
+
+#button {
+  z-index: 2;
+  position: relative;
+  width: 100%;
+  border: none;
+  background-color: rgb(162, 104, 255);
+  height: 30px;
+  color: white;
+  font-size: .8em;
+  font-weight: 500;
+  letter-spacing: 1px;
+  margin: 10px;
+  cursor: pointer;
+}
+
+#button:hover {
+  background-color: rgb(126, 84, 255);
+}
+
+.forgotLink {
+  z-index: 2;
+  font-size: .7em;
+  font-weight: 500;
+  color: rgb(44, 24, 128);
+  text-decoration: none;
+  padding: 8px 15px;
+  border-radius: 20px;
 }
 </style>
-
